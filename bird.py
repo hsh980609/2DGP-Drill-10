8c554e5645e1069963873c994e8bc2d16f44a48f
hsh980609 <130471656+hsh980609@users.noreply.github.com>
2025-11-03 14:00:22 +0900

    Bird class 생성

--- bird.py
+++ bird.py
@@ -3,31 +3,21 @@ import game_world
 import game_framework
 from state_machine import StateMachine
 
-class Fly:
-    def __init__(self, bird):
-        self.bird = bird
-        pass
-    def enter(self, e):
-        pass
-    def exit(self, e):
-        pass
-    def do(self):
-        pass
-    def draw(self):
-        if self.bird.face_dir == 1: # right
-            self.bird.image.clip_draw(int(self.bird.frame) * 100, 100, 100, 100, self.bird.x, self.bird.y)
-        else: # face_dir == -1: # left
-            self.bird.image.clip_composite_draw(int(self.bird.frame) * 100, 0, 100, 100, 0,'h' , self.bird.x, self.bird.y)
-        pass
-
-
 
 class Bird:
-    def __init__(self):
-        self.image = load_image('bird_animation.png')
+    def __init__(self, x=400, y=300):
+        if Bird.image == None:
+            Bird.image = load_image('bird_animation.png')
 
-    def update(self):
-        self.state_machine.update()
 
     def draw(self):
-        self.state_machine.draw()
+        self.image.draw(self.x, self.y)
+
+    def update(self):
+        # 위치 업데이트
+
+        # 맵 끝 충돌처리
+        if self.x < 400:
+            pass
+        elif self.x < 0:
+            pass
