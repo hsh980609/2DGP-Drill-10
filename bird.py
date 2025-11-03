9bf05374e1ad81ea87846a8e603318878a7d0739
hsh980609 <130471656+hsh980609@users.noreply.github.com>
2025-11-03 13:50:17 +0900

    fly 클래스 생성

--- bird.py
+++ bird.py
@@ -3,6 +3,19 @@ import game_world
 import game_framework
 from state_machine import StateMachine
 
+class Fly:
+    def __init__(self, bird):
+        pass
+    def enter(self, e):
+        pass
+    def exit(self, e):
+        pass
+    def do(self):
+        pass
+    def draw(self):
+        pass
+
+
 
 class Bird:
     def __init__(self):
