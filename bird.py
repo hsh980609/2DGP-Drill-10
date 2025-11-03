7c91f66eef828076c909153d76198378ca3acba7
hsh980609 <130471656+hsh980609@users.noreply.github.com>
2025-11-03 14:24:54 +0900

    새 생성 및 이동

--- bird.py
+++ bird.py
@@ -5,19 +5,20 @@ from state_machine import StateMachine
 
 
 class Bird:
-    def __init__(self, x=400, y=300):
-        if Bird.image == None:
-            Bird.image = load_image('bird_animation.png')
-
+    def __init__(self, x=0, y=300):
+        self.x, self.y = x, y
+        self.target_x = 400
+        self.speed = 100
+        Bird.image = load_image('bird_animation.png')
 
     def draw(self):
         self.image.draw(self.x, self.y)
 
     def update(self):
-        # 위치 업데이트
+        # 위치 업데이트 (목표 지점 400)
+        if self.x < self.target_x:
+            self.x += self.speed * game_framework.frame_time
 
-        # 맵 끝 충돌처리
-        if self.x < 400:
-            pass
-        elif self.x < 0:
-            pass
+        # 맵 끝 충돌 처리
+        if self.x >= self.target_x:
+            self.x = self.target_x
